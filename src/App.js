cc5cb6cf6841c00d2434b247fc8784decfde3b5a
PauloAlexandreSantos <tecnicopaulo@outlook.pt>
2021-07-21 18:35:16 +0100

    dfh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+
+
+class App extends Component {
+
+  state = {
+    counter: 0,
+    posts: []
+  };
+
+  timeoutUpdate = null;
+
+  componentDidMount() {
+
+    this.loadPosts();
+  }
+
+
+  loadPosts = async () => {
+
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+
+    const photosJson = await photos.json();
+
+
+
+    const result = postsJson.map((post, index) => {
+      return {...post, cover:  photosJson[index].url  }
+
+    });
+    this.setState({ posts: result });
+  }
+
+  render() {
+
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <div className="posts">
+          {
+            posts.map(post =>
+              <div className="post" key={post.id} >
+                <img src={post.cover} alt={post.title}/>
+                <div className="post-content">
+                  <h1>{post.id} - {post.title}</h1>
+                  <p>{post.body}</p>
+                </div>
+              </div>
+            )
+          }
+
+        </div>
+      </section>
+
+    );
+  }
+
+
 }
 
 export default App;
